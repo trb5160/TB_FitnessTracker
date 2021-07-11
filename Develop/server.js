37f7430cc1b654e635afd4239cb5027d7bd1e18c
Tom Barber <trb5160@gmail.com>
2021-07-11 19:43:41 -0400

    updated code to allow connection to Atlas

--- Develop/server.js
+++ Develop/server.js
@@ -12,10 +12,15 @@ app.use(express.json());
 app.use(express.static('public'));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
-})
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
