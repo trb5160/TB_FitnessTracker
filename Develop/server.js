f7467772aa52b10835fead4a2273908a4450dfcc
Tom Barber <trb5160@gmail.com>
2021-07-11 19:59:01 -0400

    again tried to fix code to make heroku work

--- Develop/server.js
+++ Develop/server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
     {
